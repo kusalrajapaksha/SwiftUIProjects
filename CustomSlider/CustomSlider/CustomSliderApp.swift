54dc079425bb8afa71761eb3727b14d70c4cc7e0
Kusal Rajapaksha <kusal@4axissolutions.com>
2024-01-04 21:21:48 +0530

    Initial commit

---
+++ CustomSlider/CustomSlider/CustomSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSliderApp.swift
+//  CustomSlider
+//
+//  Created by Kusal Rajapaksha on 2024-01-04.
+//
+
+import SwiftUI
+
+@main
+struct CustomSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
