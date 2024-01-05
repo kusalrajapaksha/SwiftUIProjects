1ebf153b166eb9e719427f5fffaa39d63cf557c2
Kusal Rajapaksha <kusal@4axissolutions.com>
2024-01-05 13:15:15 +0530

    DragSwapProject created

---
+++ DragSwapFeature/DragSwapFeature/DragSwapFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragSwapFeatureApp.swift
+//  DragSwapFeature
+//
+//  Created by Kusal Rajapaksha on 2024-01-05.
+//
+
+import SwiftUI
+
+@main
+struct DragSwapFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
